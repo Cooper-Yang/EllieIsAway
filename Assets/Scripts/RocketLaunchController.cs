d9ff2b5e58bcb3cf9f14b3c25dcbccba110fd174
Cooper <ty1037@nyu.edu>
2025-08-17 18:42:34 -0700

    Rocket can fly

--- Assets/Scripts/RocketLaunchController.cs
+++ Assets/Scripts/RocketLaunchController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5114b8bc30d6aefa9a98f9076432d65d78842675563e120d6d320699b0c52535
-size 11429
+oid sha256:5487f7f92c28c912e9539e093c3c5dd5cbc50c8cd78c4564243be7f9b1dea9fb
+size 17521
