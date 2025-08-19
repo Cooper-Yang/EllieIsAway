81c8a76452caea2211e25524d27d5b257c2758a1
Cooper Yang <ty1037@nyu.edu>
2025-08-18 21:37:50 -0700

    prompt is showing. bark issue

--- Assets/Scripts/SceneStartController.cs
+++ Assets/Scripts/SceneStartController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba46a390cace870b733dbf8f596f6b761c91acbc590d6a2083198ae62d37eb8b
-size 6110
+oid sha256:e6615669ccaa44de0a0a247135ef628ff73c7685ab946abe7776ee108589769d
+size 8258
